36c4d10937cb8360d3b8611d13322dead3f059f8
Hải Đăng <53001571+dangthemen10@users.noreply.github.com>
2023-09-29 22:52:17 +0700

    🚀 [Feature]: Create the cast detail page (#7)

---
+++ src/types/person.d.ts
@@ -0,0 +1,16 @@
+interface IPersonData {
+  adult: boolean;
+  also_known_as: any[];
+  biography: string;
+  birthday: string;
+  deathday: string | undefined;
+  gender: number;
+  homepage: any | undefined;
+  id: number;
+  imdb_id: string;
+  known_for_department: string;
+  name: string;
+  place_of_birth: string;
+  popularity: number;
+  profile_path: string;
+}
